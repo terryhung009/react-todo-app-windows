c005763d51b88dade541feb3de35e75af051bf4d
DannyHung <76043300+terryhung009@users.noreply.github.com>
2022-05-22 00:03:20 +0800

    feat: 畫面增加記事日期時間

---
+++ src/pages/Home/components/Edit.js
@@ -0,0 +1,13 @@
+const Edit = () =>{
+    return  <div>
+        <h1>備忘錄</h1>
+        <p>記事:</p>   
+        <input type="text"/>
+        <p>日期:</p>
+        <input type="date"/>
+        <p>時間:</p>
+        <input type="time"/>
+        </div>
+}
+
+export default Edit
